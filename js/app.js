3a7cf550f6e4aa5292c9a13e462f625bb7b34b5f
jerryshew <jerryshew@gmail.com>
2014-12-21 23:34:42 +0800

    scrolltop

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ $(function(){
   $(".first-screen .scroll-down i").click(function(){
     $("html,body").animate({scrollTop: "800px"}, 500);
   })
+
+  $(".site-badge").click(function(){
+    $("html,body").animate({scrollTop:0}, 500);
+  })
 })
