0c3693b035d894f6d8a086ba2d29a625737a2e18
jerryshew <jerryshew@gmail.com>
2014-12-28 21:32:44 +0800

    new index page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(function(){
-  $(".first-screen .scroll-down i").click(function(){
-    $("html,body").animate({scrollTop: "800px"}, 500);
-  })
+  // $(".first-screen .scroll-down i").click(function(){
+  //   $("html,body").animate({scrollTop: "800px"}, 500);
+  // })
 
-  $(".site-badge").click(function(){
-    $("html,body").animate({scrollTop:0}, 500);
-  })
+  // $(".site-badge").click(function(){
+  //   $("html,body").animate({scrollTop:0}, 500);
+  // })
 })
