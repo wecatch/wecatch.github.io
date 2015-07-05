efeae18afade8f8d340b2d23977ef0161e960de5
jerryshew <jerryshew@gmail.com>
2015-07-05 14:09:51 +0800

    add turbo logo

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-// $(function(){
-//     window.mySwipe = new Swipe(document.getElementById('open_source'), {
-//       startSlide: 2,
-//       speed: 400,
-//       auto: false,
-//       continuous: true,
-//       disableScroll: false,
-//       stopPropagation: false,
-//       callback: function(index, elem) {},
-//       transitionEnd: function(index, elem) {}
-//     });
-// })
+$(function(){
+    
+})
