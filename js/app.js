1db26617af6d605e5bc5d55f4f6483933cf7325d
jerryshew <jerryshew@gmail.com>
2016-01-11 00:29:26 +0800

    .

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 $(function(){
-    
+    $('.open-source-slide').slick({
+        autoplay: true,
+        infinite: true,
+        speed: 400,
+        cssEase: 'ease-in-out',
+        prevArrow: '<a href="javascript:;" class="pre-arr"><img src="img/right-arr.png" alt="" /></a>',
+        nextArrow: '<a href="javascript:;" class="next-arr"><img src="img/left-arr.png" alt="" /></a>',
+    });
 })
--- src/js/app.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-    window.mySwipe = Swipe($('#slider'));
-})
