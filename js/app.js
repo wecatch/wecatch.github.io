766f1e3b8bfb58a9034efc4c2b5d2fc9db3ad933
jerryshew <jerryshew@gmail.com>
2016-01-11 00:34:57 +0800

    .

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ $(function(){
         autoplay: true,
         infinite: true,
         speed: 400,
-        cssEase: 'ease-in-out',
+        fade: true,
+        // cssEase: 'ease-in-out',
         prevArrow: '<a href="javascript:;" class="pre-arr"><img src="img/right-arr.png" alt="" /></a>',
         nextArrow: '<a href="javascript:;" class="next-arr"><img src="img/left-arr.png" alt="" /></a>',
     });
