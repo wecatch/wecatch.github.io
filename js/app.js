cce389f283c14229502277841eaffd4de526b64b
jerryshew <jerryshew@gmail.com>
2016-02-17 12:01:43 +0800

    product

--- js/app.js
+++ js/app.js
@@ -3,9 +3,11 @@ $(function(){
         autoplay: true,
         infinite: true,
         speed: 400,
+        dots: true,
         fade: true,
-        // cssEase: 'ease-in-out',
-        prevArrow: '<a href="javascript:;" class="pre-arr"><img src="img/right-arr.png" alt="" /></a>',
-        nextArrow: '<a href="javascript:;" class="next-arr"><img src="img/left-arr.png" alt="" /></a>',
+        cssEase: 'ease-in-out',
+        arrows: false,
+        // prevArrow: '<a href="javascript:;" class="pre-arr"><img src="img/right-arr.png" alt="" /></a>',
+        // nextArrow: '<a href="javascript:;" class="next-arr"><img src="img/left-arr.png" alt="" /></a>',
     });
 })
